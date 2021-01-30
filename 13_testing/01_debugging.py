db32e921305b81cb679c62b8921506fd112be3b8
janobourian <supermath33@icloud.com>
2021-01-29 19:08:12 -0600

    Debugging

---
+++ 13_testing/01_debugging.py
@@ -0,0 +1,25 @@
+"""
+# Debuging
+
+# linting
+num + 4
+
+# IDE/editor
+
+# print method
+
+# read errors 
+
+# pdb python debuger
+"""
+
+import pdb
+
+
+def add(num1, num2):
+    #  pdb.set_trace()
+    t = 4*5
+    return num1 + num2
+
+
+add(4, 5)
